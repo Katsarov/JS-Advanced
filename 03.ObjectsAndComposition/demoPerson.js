17344247e83688eb3949f2ac2c6ddecaf525f2aa
Kalin Katsarov <k.l.katsarov@gmail.com>
2021-10-17 19:57:41 +0300

    New lab "Objects and Composition" added

---
+++ 03.ObjectsAndComposition/demoPerson.js
@@ -0,0 +1,23 @@
+const myPerson = {
+    name: 'Peter',
+    'last name': 'Jackson',
+    age: 23
+}
+
+
+myPerson.age = 26;
+
+console.log(myPerson);
+
+const propName = 'name';
+
+myPerson.nationality = 'UK';
+
+delete myPerson.age;
+
+console.log(myPerson[propName]);
+console.log(myPerson['last name']);
+console.log(myPerson.age);
+console.log(myPerson.nationality);
+
+console.log(myPerson);
