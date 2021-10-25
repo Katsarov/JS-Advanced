714069dc993bf97ad1e360f769335b2bc95dbee3
Kalin Katsarov <k.l.katsarov@gmail.com>
2021-10-25 19:42:57 +0300

    Added solutions for: 04. Heroic Inventory - 2 solutions 05. Lowest prices in cities

---
+++ 03.ObjectsAndCompositionExercise/04.1.HeroicInventory.js
@@ -0,0 +1,21 @@
+function solve4(arr){
+    let result = [];
+
+    arr.forEach((el) => {
+
+        let[name, level, items] = el.split(' / ');
+        result.push({
+            name: name, 
+            level: Number(level),
+            items: items ? items.split(', ') : []
+        })
+    })
+
+    console.log(JSON.stringify(result));
+
+}
+
+solve4(['Isacc / 25 / Apple, GravityGun',
+'Derek / 12 / BarrelVest, DestructionSword',
+'Hes / 1 / Desolator, Sentinel, Antara']
+);
